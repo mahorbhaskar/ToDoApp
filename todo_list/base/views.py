2653c98dcb5ff6c13821ea94309b8ea388432729
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-09-12 01:03:22 +0530

    atTemplate

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.generic.list import ListView
+from .models import Task
 # Create your views here.
 
-def taskList(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView):
+    model = Task
